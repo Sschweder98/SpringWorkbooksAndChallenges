bb17fb396729227155dac20c79d56d21a92f82c5
Sebastian <s.schweder1998@gmail.com>
2023-05-02 19:49:40 +0200

    Completed Challenge 3.2 -> Cross Field Validation

---
+++ workbook_3-2_cross_field_validation/src/main/java/com/example/workbook_31_field_validation/Workbook31FieldValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbook_31_field_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workbook31FieldValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workbook31FieldValidationApplication.class, args);
+    }
+
+}
