2b14d1dc9c3b4f66ee33afe6975209011e5cf10e
Sebastian <s.schweder1998@gmail.com>
2023-05-25 22:48:18 +0200

    solved workbook 9.1

--- workbook_9-1_CRUD/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ workbook_9-1_CRUD/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,11 +1,34 @@
 package com.ltp.gradesubmission.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ltp.gradesubmission.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    @RequestMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id){
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 
+    @PostMapping("/")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Student>> getStudents(){
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 }
